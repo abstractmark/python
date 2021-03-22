8a1fccf3cb2d0c43e5cc1f6a2cd08016dee42c78
kimlimjustin <kimlimjustin@gmail.com>
2021-03-22 15:08:53 +0700

    Created basic structure

---
+++ src/tokenizer.py
@@ -0,0 +1,6 @@
+function Tokenize(abstractmark){
+    tokenizedData = [];
+    data = abstractmark.split('\n')
+    #List to determine ammount of tab regardless the indentation
+    totalSpaceArray = []
+}
