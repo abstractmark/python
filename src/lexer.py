a498a53da88b27e01e7857d4ee6b7076fecfe2c6
kimlimjustin <kimlimjustin@gmail.com>
2021-04-03 22:44:54 +0700

    Fixed lexer bugs

--- src/lexer.py
+++ src/lexer.py
@@ -1,5 +1,9 @@
 import sys
 import re
+
+def isMatch(regex, string):
+    return bool(re.search(regex, string))
+
 def Lex(tokenizedData):
     lexedData = []
     for data in tokenizedData:
@@ -7,53 +11,53 @@ def Lex(tokenizedData):
         # Showing what each line includes
         newData["includes"] = {};
         # Check whether the line contains heading ID
-        if(re.match(r"(.*?)\{\#(.*?)\}", data["value"])): newData["includes"]["headingId"] = True
+        if(isMatch(r"(.*?)\{\#(.*?)\}", data["value"])): newData["includes"]["headingId"] = True
         else: newData["includes"]["headingId"] = False
         # Check whether the line contains image
-        if(re.match(r"!\[[^\]]*\]\((.*?)\s*(\"(?:.*[^\"])\")?\s*\)", data["value"])): newData["includes"]["image"] = True
+        if(isMatch(r"!\[[^\]]*\]\((.*?)\s*(\"(?:.*[^\"])\")?\s*\)", data["value"])): newData["includes"]["image"] = True
         else: newData["includes"]["image"] = False
         # Check whether the line contains link
-        if(re.match(r"(?<!\!)\[.+\]\(.+\)", data["value"])): newData["includes"]['link'] = True
+        if(isMatch(r"(?<!\!)\[.+\]\(.+\)", data["value"])): newData["includes"]['link'] = True
         else: newData["includes"]["link"] = False
         # Check whether the line contains table
-        if(re.match(r"\|(.*?)\|", data["value"])): newData["includes"]["table"] = True
+        if(isMatch(r"\|(.*?)\|", data["value"])): newData["includes"]["table"] = True
         else: newData["includes"]["table"] = False
         # Check whether the line contains task list
-        if(re.match(r"^- \[[xX ]\] \S+", data["value"])): newData["includes"]["taskList"] = True
+        if(isMatch(r"^- \[[xX ]\] \S+", data["value"])): newData["includes"]["taskList"] = True
         else: newData["includes"]["taskList"] = False
         # Check whether the line is class definition
         newData["includes"]["defineClass"] = True if data["value"] == "---define" else False
         # Check whether the line is Thematic Break
         newData["includes"]["horizontalRule"] = True if data["value"] == "---" else False
         # Check whether the line is a fenced code block
-        if(re.match(r"^\`\`\`", data["value"])): newData["includes"]["fencedCodeBlock"] = True
+        if(isMatch(r"^\`\`\`", data["value"])): newData["includes"]["fencedCodeBlock"] = True
         else: newData["includes"]["fencedCodeBlock"] = False
         # Check whether the line is Ordered List
-        if(re.match(r"^\d+\. .", data["value"])): newData["includes"]["orderedList"] = True
+        if(isMatch(r"^\d+\. .", data["value"])): newData["includes"]["orderedList"] = True
         else: newData["includes"]["orderedList"] = False
         # Check whether the line is Unordered List
-        if(re.match(r"^- ((?!\[[xX ]\]).)*$", data["value"])): newData["includes"]["unorderedList"] = True
+        if(isMatch(r"^- ((?!\[[xX ]\]).)*$", data["value"])): newData["includes"]["unorderedList"] = True
         else: newData["includes"]["unorderedList"] = False
         # Check whether the line is heading
-        if(re.match(r"^#{1,6} .", data["value"])): newData["includes"]["heading"] = True
+        if(isMatch(r"^#{1,6} .", data["value"])): newData["includes"]["heading"] = True
         else: newData["includes"]["heading"] = False
         # Check whether the line is a blockquote
-        if(re.match(r"^>+ .", data["value"])): newData["includes"]["blockquote"] = True
+        if(isMatch(r"^>+ .", data["value"])): newData["includes"]["blockquote"] = True
         else: newData["includes"]["blockquote"] = False
         # Check whether the line is including external css
-        if(re.match(r"stylesheet\s*(=|:)", data["value"])): newData["includes"]["stylesheet"] = True
+        if(isMatch(r"stylesheet\s*(=|:)", data["value"])): newData["includes"]["stylesheet"] = True
         else: newData["includes"]["stylesheet"] = False
         # Check whether the line is including external javascript
-        if(re.match(r"script\s*(=|:)", data["value"])): newData["includes"]["externalScript"] = True
+        if(isMatch(r"script\s*(=|:)", data["value"])): newData["includes"]["externalScript"] = True
         else: newData["includes"]["externalScript"] = False
         # Check whether the line is marquee tag
-        if(re.match(r"(<~|~>)\s*(.*?)", data["value"])): newData["includes"]["marquee"] = True
+        if(isMatch(r"(<~|~>)\s*(.*?)", data["value"])): newData["includes"]["marquee"] = True
         else: newData["includes"]["marquee"] = False
-        if(re.match(r"(.*?)\{((?!(\#|\!|\.)))(.*?)\}", data["value"])): newData["includes"]["inlineStyle"] = True
+        if(isMatch(r"(.*?)\{((?!(\#|\!|\.)))(.*?)\}", data["value"])): newData["includes"]["inlineStyle"] = True
         # Check whether the line contains inline style
         else: newData["includes"]["inlineStyle"] = False
         # Showing whether the line contains class usage
-        if(re.match(r"(.*?)\{\.(.*?)\}", data["value"])): newData["includes"]["classUsage"] = True
+        if(isMatch(r"(.*?)\{\.(.*?)\}", data["value"])): newData["includes"]["classUsage"] = True
         else: newData["includes"]["classUsage"] = False
         # Push the into array
         lexedData.append(newData)
